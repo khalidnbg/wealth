bc2d2842ef417ce176b80ad90e72958685d1d89a
khalidnbg <khalidnabgaoui4@gmail.com>
2025-11-23 19:54:49 +0100

    setup routes and add react spinner

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DashboardPage = () => {
+  return (
+    <div className="space-y-8">
+      {/* Budget Progress */}
+
+      {/* Dashboard Overview */}
+
+      {/* Accounts Grid */}
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ app/(main)/layout.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MainLayout = ({ children }) => {
+  // redirect user after onboarding
+
+  return <div className="container mx-auto my-32">{children}</div>;
+};
+
+export default MainLayout;
