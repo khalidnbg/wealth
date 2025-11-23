c9a0e8c7d6a9ef7df383cb15c4b4df9d22fd19c2
khalidnbg <khalidnabgaoui4@gmail.com>
2025-11-23 13:27:29 +0100

    Add Landing page Add initial database schema diagram for Users, Accounts, Transactions, and Budgets

--- app/layout.js
+++ app/layout.js
@@ -14,11 +14,16 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <link rel="icon" href="/logo-sm.png" sizes="any" />
+        </head>
+
         <body className={`${inter.className}`}>
           {/* header */}
           <Header />
 
           <main className="min-h-screen">{children}</main>
+
           {/* footer */}
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto text-center text-gray-600">
