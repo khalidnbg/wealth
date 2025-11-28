9a130a72361bcbcf3d8ba14279a80c1cdb260661
khalidnbg <khalidnabgaoui4@gmail.com>
2025-11-28 22:43:04 +0100

    feat: implement transaction creation functionality with form handling and validation

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,7 +1,20 @@
+import { getUserAccounts } from "@/actions/dashboard";
+import { defaultCategories } from "@/data/categories";
 import React from "react";
+import AddTransactionForm from "../_components/transaction-form";
 
-const CreateTransaction = () => {
-  return <div>CreateTransaction</div>;
+const AddTransactionPage = async ({ searchParams }) => {
+  const accounts = await getUserAccounts();
+
+  return (
+    <div className="max-w-3xl mx-auto px-5">
+      <div className="flex justify-center md:justify-normal mb-8">
+        <h1 className="text-5xl gradient-title ">Add Transaction</h1>
+      </div>
+
+      <AddTransactionForm accounts={accounts} categories={defaultCategories} />
+    </div>
+  );
 };
 
-export default CreateTransaction;
+export default AddTransactionPage;
